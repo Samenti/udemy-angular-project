3dc0266917c232c14e7554c39352f34524b26155
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2022-12-15 18:07:12 +0200

    my solution to manual navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+export type CurrentTab = 'shopping-list' | 'recipes';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  currentTab: CurrentTab = 'recipes';
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { CurrentTab } from '../app.component';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
+  @Output() navButtonClicked = new EventEmitter<CurrentTab>();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  public onNavClick(navLink: CurrentTab) {
+    this.navButtonClicked.emit(navLink);
+  }
 }
