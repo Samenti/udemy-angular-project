30f5360f3edc4d7d7bb7197213bb82868f6e6807
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2023-02-06 17:52:04 +0200

    delete ingredient button, unsubscribe in recipe list

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -64,6 +64,10 @@ export class RecipeEditComponent implements OnInit {
     );
   }
 
+  onDeleteIngredient(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
+
   onCancel(): void {
     this.router.navigate(['../'], { relativeTo: this.route });
   }
