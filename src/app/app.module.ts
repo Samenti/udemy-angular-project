8af120b941443b3416e0535c6203283cd51c66d7
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2023-02-15 20:20:33 +0200

    storing and fetching recipes with firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -30,7 +31,13 @@ import { RecipeService } from './recipes/recipe.service';
     RecipeStartComponent,
     RecipeEditComponent,
   ],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ],
   providers: [RecipeService],
   bootstrap: [AppComponent],
 })
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes(): void {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        'https://udemy-angular-project-d838f-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+        recipes
+      )
+      .subscribe((response) => console.log(response));
+  }
+
+  fetchRecipes(): void {
+    this.http
+      .get<Recipe[]>(
+        'https://udemy-angular-project-d838f-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
+      )
+      .pipe(
+        // set the ingredients to an empty array if it doesn't exist
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
+      )
+      .subscribe((recipes) => {
+        this.recipeService.setRecipes(recipes);
+      });
+  }
+}
