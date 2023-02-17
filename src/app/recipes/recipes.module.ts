abf8a4308b898724207fbfafb74e7a93328f2af4
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2023-02-17 22:22:05 +0200

    added a separate routing module for the recipes feature

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipesRoutingModule } from './recipes-routing.module';
 import { RecipesComponent } from './recipes.component';
 
 @NgModule({
@@ -19,14 +19,8 @@ import { RecipesComponent } from './recipes.component';
     RecipeEditComponent,
   ],
   // unclear if CommonModule is actually needed or not
-  imports: [CommonModule, RouterModule, ReactiveFormsModule],
-  exports: [
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    RecipeStartComponent,
-    RecipeEditComponent,
-  ],
+  imports: [CommonModule, RecipesRoutingModule, ReactiveFormsModule],
+  // exports are no longer necessary, since the RecipesRoutingModule
+  // gets access to the declarations of this module
 })
 export class RecipesModule {}
