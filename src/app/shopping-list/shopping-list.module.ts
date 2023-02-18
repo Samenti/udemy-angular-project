d6554a3d91824d6b503d0d3373ec8e7f8d4c9e72
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2023-02-18 18:44:28 +0200

    demonstrating multiple instances of a provided service

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -5,9 +5,11 @@ import { ShoppingListComponent } from './shopping-list.component';
 import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
+// import { LoggingService } from '../logging.service';
 
 @NgModule({
   declarations: [ShoppingListComponent, ShoppingEditComponent],
   imports: [SharedModule, FormsModule, ShoppingListRoutingModule],
+  // providers: [LoggingService]
 })
 export class ShoppingListModule {}
